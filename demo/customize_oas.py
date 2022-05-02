1bcfa1ae3b876e0d7289e31509c1a342fe8b9deb
Andreas Molzer <andreas.molzer@nebumind.com>
2022-05-02 13:31:41 +0200

    Add documentation for __modify_schema__

--- demo/customize_oas.py
+++ demo/customize_oas.py
@@ -14,29 +14,26 @@ This pet is a good boy and deserves lots of pets
 '''
 
 
-def add_markdown_response(func):
-    def modify(oas, oas_path, view, oas_operation):
-        oas.components.examples.setdefault('dog-example', {
-                'summary': 'A detailed listing for a dog',
-                'value': DOG_DESCRIPTION,
-            })
+def add_custom_dog_example(oas, oas_path, view, oas_operation):
+    oas.components.examples.setdefault('dog-example', {
+            'summary': 'A detailed listing for a dog',
+            'value': DOG_DESCRIPTION,
+        })
 
-        oas_operation.responses[200].content = {
-                'text/markdown': {
-                    'examples': {
-                        'dog': {
-                            '$ref': '#/components/examples/dog-example'
-                        }
+    oas_operation.responses[200].content = {
+            'text/markdown': {
+                'examples': {
+                    'dog': {
+                        '$ref': '#/components/examples/dog-example'
                     }
                 }
             }
+        }
 
-    func.__modify_schema__ = modify
     return func
 
 
 class PetDetailsView(PydanticView):
-    @add_markdown_response
     async def get(self, id: int, /) -> r200:
         """
         Show a detailed listing describing the pet.
@@ -48,3 +45,6 @@ class PetDetailsView(PydanticView):
         pet = self.request.app["model"].find_pet(id)
         body = DOG_DESCRIPTION
         return web.Response(content_type='text/markdown', body=body)
+
+
+    get.__modify_schema__ = add_custom_dog_example
